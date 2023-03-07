adfdb14229e86fd3b34b74b58f7ba4ae9158d66d
ankakaito.OatMeaL <ankakaito.ankakaito880123@gmail.com>
2023-03-07 16:51:03 +0700

    Fix Transaction Line

--- models/book_transaction.py
+++ models/book_transaction.py
@@ -17,7 +17,6 @@ class BookTransactionLine(models.Model):
     #             rec.book_title = rec.management_id.book_id.name
 
     def get_excel_report(self):
-
         return {
             'type': 'ir.actions.act_url',
             'url': '/collection_book/book_transaction_report_excel/%s' % (self.id),
@@ -40,7 +39,6 @@ class BookTransaction(models.Model):
     _name = 'book.transaction'
     _description = 'Book Transaction'
 
-
     def func_to_approve(self):
         if self.status == 'draft':
             if len(self.transaction_ids) == 0:
@@ -48,6 +46,7 @@ class BookTransaction(models.Model):
             else:
                 self.status = 'to_approve'
                 self.validation = "processing"
+
     def func_approved(self):
         if self.status == 'to_approve':
             if len(self.transaction_ids) == 0:
@@ -74,21 +73,14 @@ class BookTransaction(models.Model):
                             self.validation = 'borrowed'
                         #transaction.management_id.write({'amount_borrowed': 0})
 
-   # def get_excel_report(self):
-   #     return {
-   #          'type': 'ir.actions.act_url',
-   #          'url': '/collection_book/book_transaction_report_execl/%s' % (self.id),
-   #          'target': 'new',
-   #     }
-
-    #@api.multi
     def get_excel_report(self):
         return self.env.ref('collection_book.report_master_transaction_views').report_action(self)
 
     name = fields.Char(string="Transaction Number", default='New')
     member_id = fields.Many2one('member',"Member Name")
     borrowing_date = fields.Date(string="Borrowing Date", default=fields.Date.today())
-    returning_date= fields.Datetime(string="Returning Date")
+    returning_date = fields.Date(string="Returning Date", default=fields.Date.today())
+    number_of_days = fields.Integer(string="Number Of Days")
     transaction_ids = fields.One2many('book.transaction.line','transaction_id',"List Book Management", ondelete='cascade')
     status = fields.Selection([('draft','Draft'),('to_approve','To Approve'),('approved','Approved'),('done','Done')], default='draft')
     validation = fields.Selection([('processing','Processing'),('borrowed','Borrowed'),('returned','Returned')], string="Transaction Status",  default='processing')
@@ -98,8 +90,31 @@ class BookTransaction(models.Model):
         if vals.get('name', 'New') == 'New':
             vals['name'] = self.env['ir.sequence'].next_by_code(
                 'seq.book.transaction') or 'New'
+        if not vals.get('transaction_ids'):
+            raise ValidationError(_( " Please Fill The Transaction Line"))
         result = super(BookTransaction, self).create(vals)
         return result
+
+    def write(self, vals):
+        if not vals.get('transaction_ids'):
+            raise ValidationError(_( " Please Fill The Transaction Line"))
+        else:
+            if len(vals['transaction_ids']) == 1:
+                available_line = True
+                if vals['transaction_ids'][0][0] == 2:
+                    available_line = False
+                if not available_line:
+                    raise ValidationError(_( " Please Fill The Transaction Line"))
+            else:
+                available_line = False
+                for line in vals['transaction_ids']:
+                    if line[0] != 2:
+                        available_line = True
+                        break
+                if not available_line:
+                    raise ValidationError(_( " Please Fill The Transaction Line"))
+        result = super(BookTransaction, self).write(vals)
+        return result   
     
     @api.onchange('transaction_ids')
     def _onchange_management_id(self):
@@ -114,3 +129,18 @@ class BookTransaction(models.Model):
             if line.management_id.id == management_id and line.id != id:
                 return True
         return False
+    
+    @api.constrains('member_id')
+    def _empty_field(self):
+        if not self.member_id:
+            raise ValidationError(_( " Please Select The Member Name "))
+    
+    @api.constrains('returning_date','borrowing_date')
+    def _check_same_date(self):
+        if self.borrowing_date == self.returning_date:
+            raise ValidationError(_( " Returning Date Cannot Be same with borrowing Date "))
+    
+    @api.onchange('returning_date','borrowing_date')
+    def _count_onchange_days(self):
+        date = self.returning_date - self.borrowing_date
+        self.number_of_days = abs(date.days)
